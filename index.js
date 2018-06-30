c71750cad02a78e5b0158b8cbdec14f676b0d1e7
cice <cice@08User06.CICE.LOCAL>
2018-06-30 10:24:30 +0200

    ahora con nodemon

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const winston = require('winston');
+
+let validUrl = ("/index.html", "/bio.html", "portfolio.html", "contacto.html");
+
+
 
 http.createServer(function (request, response) {
     console.log('request ', request.url);
 
-    var filePath = '.' + request.url;
+    let filePath = '.' + request.url;
     if (filePath == './') {
         filePath = './index.html';
     }
 
-    var extname = String(path.extname(filePath));
-    var mimeTypes = {
+    let extname = String(path.extname(filePath));
+    let mimeTypes = {
         '.html': 'text/html',
         '.css': 'text/css',
         '.png': 'image/png',
         '.jpg': 'image/jpg',
     };
 
-    var contentType = mimeTypes[extname] || 'website';
+    let contentType = mimeTypes[extname] || 'website';
 
     fs.readFile(filePath, function(error, content) {
         if (error);
